f92edadf25ce3a3f1211b9df6721ff0fdaed86e1
juanpablodejesus <juanpablodejesus_figueroa@ucol.mx>
2020-03-11 09:00:02 -0600

    Initial commit

---
+++ app/src/main/java/com/example/gato/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.gato;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button[][] buttons = new Button[3][3];
+
+    private boolean Turno = true;
+    private int jugada;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String buttonID = "button_" + i + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
+                buttons[i][j].setOnClickListener(this);
+            }
+        }
+        Button buttonReset = findViewById(R.id.button_reset);
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (!((Button) v).getText().toString().equals("")) {
+            return;
+        }
+
+        if (Turno) {
+            ((Button) v).setText("X");
+        } else {
+            AlertDialog.Builder alerta = new AlertDialog.Builder(MainActivity.this);
+            alerta.setMessage("Es turno de la maquina espera!")
+                    .setCancelable(true);
+            AlertDialog titulo = alerta.create();
+            titulo.setTitle("Alerta");
+            titulo.show();
+        }
+
+        jugada++;
+
+        if (checkForWin()) {
+            if (Turno) {
+                Jugador1Gana();
+            } else {
+                CompuGana();
+            }
+        } else if (jugada == 9) {
+            draw();
+        } else {
+            Turno = !Turno;
+        }
+    }
+    private boolean checkForWin() {
+        String[][] field = new String[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = buttons[i][j].getText().toString();
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(field[i][1])
+                    && field[i][0].equals(field[i][2])
+                    && !field[i][0].equals("")) {
+                return true;
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (field[0][i].equals(field[1][i])
+                    && field[0][i].equals(field[2][i])
+                    && !field[0][i].equals("")) {
+                return true;
+            }
+        }
+
+        if (field[0][0].equals(field[1][1])
+                && field[0][0].equals(field[2][2])
+                && !field[0][0].equals("")) {
+            return true;
+        }
+
+        if (field[0][2].equals(field[1][1])
+                && field[0][2].equals(field[2][0])
+                && !field[0][2].equals("")) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void Jugador1Gana() {
+    }
+
+    private void CompuGana() {
+    }
+
+    private void draw() {
+    }
+}
+
---
+++ app/src/main/java/com/example/gato/Onclick.java
@@ -0,0 +1,8 @@
+package com.example.gato;
+
+class Onclick extends MainActivity {
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
